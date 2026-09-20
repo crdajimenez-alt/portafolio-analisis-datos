793e63dd183ac6006fa389ab7fcbd5240a010b4a
crdajimenez-alt <crdajimenez@utp.edu.co>
2026-09-19 20:46:40 -0500

    Update analisis_ventas.sql

--- SQL/proyecto_ventas/analisis_ventas.sql
+++ SQL/proyecto_ventas/analisis_ventas.sql
@@ -90,3 +90,27 @@ SELECT
     ) AS ranking
 FROM gastos_clientes
 ORDER BY ranking;
+
+-- ==========================================
+-- 6. Comparación de ventas consecutivas
+-- ==========================================
+
+WITH analisis_ventas AS (
+    SELECT
+        id_venta,
+        producto,
+        cantidad * precio AS total_venta,
+        LAG(cantidad * precio) OVER (
+            ORDER BY id_venta
+        ) AS venta_anterior
+    FROM ventas
+)
+
+SELECT
+    id_venta,
+    producto,
+    total_venta,
+    venta_anterior,
+    total_venta - venta_anterior AS cambio
+FROM analisis_ventas
+ORDER BY id_venta;
