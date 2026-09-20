3bce89192b2c48ac9267f1dee7fd4b475bcb6cd1
crdajimenez-alt <crdajimenez@utp.edu.co>
2026-09-19 20:09:54 -0500

    Update analisis_ventas.sql

--- SQL/proyecto_ventas/analisis_ventas.sql
+++ SQL/proyecto_ventas/analisis_ventas.sql
@@ -1,1 +1,16 @@
+-- ==========================================
+-- PROYECTO: ANÁLISIS DE VENTAS
+-- Herramienta: PostgreSQL
+-- ==========================================
 
+
+-- 1. Gasto total de cada cliente
+
+SELECT
+    c.nombre,
+    COALESCE(SUM(v.cantidad * v.precio), 0) AS total_gastado
+FROM clientes AS c
+LEFT JOIN ventas AS v
+    ON c.id_cliente = v.id_cliente
+GROUP BY c.nombre
+ORDER BY total_gastado DESC;
